e755852b265b14040f771f119714337ff4e6306a
madaniajogja <90940954+madaniajogja@users.noreply.github.com>
2021-11-21 10:47:40 +0700

    Update robot-game-01.js

--- robot-game-01.js
+++ robot-game-01.js
@@ -67,18 +67,4 @@ function component(width, height, color, x, y, type) {
     }
 }
 
-function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;    
-	
-	//eval (teks);
-	//alert (teks);
-	
-    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
-    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
-    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
-    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
-    myGamePiece.newPos();    
-    myGamePiece.update();
-}
+
