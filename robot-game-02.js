6a02bc22f1d4e9e53cbac772bc730e59b17239e4
madaniajogja <90940954+madaniajogja@users.noreply.github.com>
2021-11-22 00:28:43 +0700

    Create robot-game-02.js

---
+++ robot-game-02.js
@@ -0,0 +1,80 @@
+var myGamePiece;
+
+var newImg = new Image();
+
+
+
+newImg.src = 'https://lh3.googleusercontent.com/sL49KPZhvCAP2lq_5j0tykl67KrvhQlePrG_KQ0Ho9uJD_vcvxHolMP6wCYN0VX5_G8LT9n6m3BdKlKWAQ=w77-h110';
+
+function startGame() {
+    myGamePiece = new component(30, 30, 'https://lh3.googleusercontent.com/sL49KPZhvCAP2lq_5j0tykl67KrvhQlePrG_KQ0Ho9uJD_vcvxHolMP6wCYN0VX5_G8LT9n6m3BdKlKWAQ=w77-h110', 10, 120, "image");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 230;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+		
+		window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+		
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (type == "image") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;    
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();    
+    myGamePiece.update();
+}
